fe7cf2e8d7f7ce7333ef198a47cd3ac1c2f4abbd
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2019-08-17 12:27:19 +0100

    Initial Commit

---
+++ SafetyOnSite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafetyOnSite
+//
+//  Created by M'haimdat omar on 17-08-2019.
+//  Copyright © 2019 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
